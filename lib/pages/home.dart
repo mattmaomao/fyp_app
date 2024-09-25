be6aa32669606a8377c690145531687252bd56b9
mattmaomao <matthewcp220@gmail.com>
2024-09-25 17:16:30 +0800

    add animation to home => sub pages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,66 +14,82 @@ class HomePage extends StatelessWidget {
     return ListView(
       children: [
         _buildHorizontalBar(
-            context, 'Archery Introduction', 'assets/func_banner_temp.jpg', () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ArcheryIntroPage()),
-          );
-        }),
+          context,
+          'Archery Introduction',
+          'assets/func_banner_temp.jpg',
+          ArcheryIntroPage(),
+        ),
         _buildHorizontalBar(
-            context, 'Slow Motion Camera', 'assets/func_banner_temp.jpg', () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SlowMoCamPage()),
-          );
-        }),
+          context,
+          'Slow Motion Camera',
+          'assets/func_banner_temp.jpg',
+          SlowMoCamPage(),
+        ),
         _buildHorizontalBar(
-            context, 'Delay Camera', 'assets/func_banner_temp.jpg', () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DelayCamPage()),
-          );
-        }),
+          context,
+          'Delay Camera',
+          'assets/func_banner_temp.jpg',
+          DelayCamPage(),
+        ),
         _buildHorizontalBar(
-            context, 'Posture Detection', 'assets/func_banner_temp.jpg', () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => PostureDetectionPage()),
-          );
-        }),
+          context,
+          'Posture Detection',
+          'assets/func_banner_temp.jpg',
+          PostureDetectionPage(),
+        ),
         _buildHorizontalBar(
-            context, 'Score Notes', 'assets/func_banner_temp.jpg', () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ScoreNotesPage()),
-          );
-        }),
+          context,
+          'Score Notes',
+          'assets/func_banner_temp.jpg',
+          ScoreNotesPage(),
+        ),
         _buildHorizontalBar(
-            context, 'Score Analysis', 'assets/func_banner_temp.jpg', () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ScoreAnalysisPage()),
-          );
-        }),
-        _buildHorizontalBar(context, 'Timer', 'assets/func_banner_temp.jpg',
-            () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => TimerPage()),
-          );
-        }),
-        // _buildHorizontalBar(context, 'Temp', 'assets/func_banner_temp.jpg',
-        //     () {
-        //   // Navigate to Function
-        // }),
+          context,
+          'Score Analysis',
+          'assets/func_banner_temp.jpg',
+          ScoreAnalysisPage(),
+        ),
+        _buildHorizontalBar(
+          context,
+          'Timer',
+          'assets/func_banner_temp.jpg',
+          TimerPage(),
+        ),
+        // _buildHorizontalBar(
+        //   context,
+        //   'Temp',
+        //   'assets/func_banner_temp.jpg',
+        //   TimerPage(),
+        // ),
       ],
     );
   }
 
   Widget _buildHorizontalBar(
-      BuildContext context, String text, String imagePath, VoidCallback onTap) {
+      BuildContext context, String text, String imagePath, Widget targetPage) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: () {
+        Navigator.of(context).push(
+          // Slide transition from right to left
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) => targetPage,
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              var begin = Offset(1.0, 0.0); // from right
+              var end = Offset.zero; // to top-left
+              var curve = Curves.ease;
+              // animation
+              var tween =
+                  Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
+            },
+          ),
+        );
+      },
       child: Container(
         height: 64,
         margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
