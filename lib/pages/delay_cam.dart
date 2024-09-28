807b46131e5caa9b51c782b22045b56b3adec7ea
cltong2 <1155176563@link.cuhk.edu.hk>
2024-09-28 16:14:43 +0800

    Update delay_cam.dart
    Implemented camera only

--- lib/pages/delay_cam.dart
+++ lib/pages/delay_cam.dart
@@ -2,58 +2,32 @@
 import 'package:flutter/material.dart';
 import 'package:fyp_app/utils/header_with_title.dart';
 import 'package:camera/camera.dart';
+late List<CameraDescription> cameras;
 
-/*
-class DelayCamPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: HeaderWithTitle(title: 'Delay Camera'),
-      body: Center(
-        child: Text('Functionality to be implemented'),
-      ),
-    );
-  }
-}
-*/
 
 class DelayCamPage extends StatefulWidget {
+  const DelayCamPage({super.key});
+
   @override
-  _DelayCamPageState createState() => _DelayCamPageState();
+  State<DelayCamPage> createState() => _DelayCamPageState();
 }
 
 class _DelayCamPageState extends State<DelayCamPage>{
-  CameraController? _controller;
-  Future<void>? _initializeControllerFuture;
+
+  late CameraController cameracontroller;
+  late Future<void> cameravalue;
 
   @override
   void initState() {
     super.initState();
     print('Initializing cameras...');
-    availableCameras().then((cameras) {
-      print('Cameras available: ${cameras.length}');
-      if (cameras.isNotEmpty){
-       final camera = cameras.first;
-        _controller = CameraController(camera, ResolutionPreset.high,);
-        print('Initializing camera controller...');
-
-      _initializeControllerFuture = _controller!.initialize().then((_){
-        print('Camera initialized successfully.');
-      }).catchError((error){
-        print('Error initializing camera: $error');
-      });
-      } else {
-        print('No cameras found');
-      }
-    }).catchError((error){
-      print('Error getting cameras: $error');
-    });
+    cameracontroller = CameraController(cameras[0], ResolutionPreset.max);
+    cameravalue = cameracontroller.initialize();
   }    
-  
 
   @override
   void dispose() {
-    _controller?.dispose();
+    cameracontroller.dispose();
     super.dispose();
   }
 
@@ -61,25 +35,16 @@ class _DelayCamPageState extends State<DelayCamPage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HeaderWithTitle(title: 'Delay Camera'),
-      body: FutureBuilder<void>(future: _initializeControllerFuture, builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done){
-          return CameraPreview(_controller!);
+      body: Stack(children: [
+        FutureBuilder(future: cameravalue, builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done){
+          return CameraPreview(cameracontroller);
         } else {
-        //  return Center(child: CircularProgressIndicator());
-        return Center(child: Text('Camera preview is unavailable'));
+          return Center(child: CircularProgressIndicator());
         }
-      },),
-/*
-      floatingActionButton: FloatingActionButton(onPressed: () async {
-        try {
-          await _initializeControllerFuture;
-          final image = await _controller!.takePicture();
-          print('Picture saved');
-        } catch (e){
-          print(e);
-        }
-      }, child: Icon(Icons.camera),),
-      */
+        },)
+      ])
+      
     );
   }
 }
